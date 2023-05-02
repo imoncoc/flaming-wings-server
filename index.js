832385a1b2d4a0df482fa45fd0148b2e58918eb6
Md Imon Hossain <imoncoc0@gmail.com>
2023-05-02 13:42:34 +0600

    added Chef data

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const app = express();
 const cors = require('cors')
 const port = process.env.port || 5000;
 
+const chef = require('./data/chef.json')
+
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Flaming Wings is Running");
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
 })
 
 app.listen(port, () => {
